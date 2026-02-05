84ed874dee47e8e17bfdea1303fc6cf0b081bcbf
sid314 <sidaa314@gmail.com>
2026-02-05 23:33:57 +0530

    slopsite assemble

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/personal-slopsite/",
   server: {
     host: true, // <--- ADD THIS LINE
   },

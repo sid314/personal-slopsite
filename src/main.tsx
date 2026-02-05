a88b39b431061b642a72fa33982edabd2a1aac5b
sid314 <sidaa314@gmail.com>
2026-02-05 23:38:22 +0530

    slopsite happy ?

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // <--- CHANGE THIS (was BrowserRouter)
 import App from "./App.tsx";
 import "./index.css";
 import "devicon/devicon.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      {/* We removed <Routes> here. App handles routing now. */}
+    <HashRouter>
+      {" "}
+      {/* <--- CHANGE THIS */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
